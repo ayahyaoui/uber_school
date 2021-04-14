aca688eb5dab43847660ff720539717106853b25
EleniPistiloglou <72700267+EleniPistiloglou@users.noreply.github.com>
2021-04-14 09:57:01 +0200

    Add files via upload

---
+++ WebContent/client.js
@@ -0,0 +1,10 @@
+let div1 = document.createElement("DIV");
+div1.setAttribute("class", "left_div");
+div1.setAttribute("style", "background-color : #993399 ; width : 35% ; height : 100vh ; padding : 100px ; border : 0px ; margin : 0")
+let txt1 = document.createElement("H1");
+txt1.setAttribute("class", "left_title");
+txt1.setAttribute("style", "font-size:560%");
+txt1.setAttribute("style","font-family : Tahoma");
+txt1.textContent="Uber School";
+div1.appendChild(txt1);
+document.body.appendChild(div1);
